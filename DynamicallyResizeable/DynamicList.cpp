27b3ef1dc792402856ac54bcf2efd2eb75d705a7
Jack Scherner <89797373+scherner5@users.noreply.github.com>
2021-11-04 12:51:27 -0400

    Final Update v2

--- DynamicallyResizeable/DynamicList.cpp
+++ DynamicallyResizeable/DynamicList.cpp
@@ -4,41 +4,4 @@
 
 using namespace std;
 
-template <class j>
-DynamicList<j>::DynamicList()
-{
-	counter = 0;
-	list = new j[8];
-	listSize = 8;
-}
-
-template <class j>
-void DynamicList<j>::push(j x) {
-	if ((listSize - 1) > counter) {
-		list[counter] = x;
-		counter++;
-	}
-	else {
-		listSize *= 2;
-		j* newBiggerList = new j[listSize];
-		for (int i = 0; i < counter; i++) {
-			newBiggerList[i] = list[i];
-		}
-		list = newBiggerList;
-		list[counter] = x;
-		counter++;
-	}
-}
-
-template <class j>
-int DynamicList<j>::length() {
-	return counter;
-}
-
-template <class j>
-int DynamicList<j>::get(int i) {
-	return list[i];
-}
-
-
 
--- DynamicallyResizeable/DynamicList.h
+++ DynamicallyResizeable/DynamicList.h
@@ -22,3 +22,39 @@ void DynamicList<j>::remove(int y) {
 	counter--;
 }
 
+template <class j>
+DynamicList<j>::DynamicList()
+{
+	counter = 0;
+	list = new j[8];
+	listSize = 8;
+}
+
+template <class j>
+void DynamicList<j>::push(j x) {
+	if ((listSize - 1) > counter) {
+		list[counter] = x;
+		counter++;
+	}
+	else {
+		listSize *= 2;
+		j* newBiggerList = new j[listSize];
+		for (int i = 0; i < counter; i++) {
+			newBiggerList[i] = list[i];
+		}
+		list = newBiggerList;
+		list[counter] = x;
+		counter++;
+	}
+}
+
+template <class j>
+int DynamicList<j>::length() {
+	return counter;
+}
+
+template <class j>
+int DynamicList<j>::get(int i) {
+	return list[i];
+}
+
