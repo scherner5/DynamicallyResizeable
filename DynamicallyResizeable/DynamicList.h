3df7df99faacefe715e03ca8b242a06c0968988a
Jack Scherner <89797373+scherner5@users.noreply.github.com>
2021-11-09 16:39:44 -0500

    Add index and contains method

--- DynamicallyResizeable/DynamicList.h
+++ DynamicallyResizeable/DynamicList.h
@@ -1,20 +1,22 @@
 #pragma once
 
-template <class j>
+template <typename j>
 class DynamicList
 {
 public:
 	DynamicList();
-	j counter;
-	j listSize;
+	int counter;
+	int listSize;
 	j* list;
 	void push(j x);
 	int length();
+	int index(j y);
 	void remove(int y);
 	int get(int i);
+	bool contains(j z);
 };
 
-template <class j>
+template <typename j>
 void DynamicList<j>::remove(int y) {
 	for (int c = y; c < counter; c++) {
 		list[c] = list[c + 1];
@@ -22,7 +24,7 @@ void DynamicList<j>::remove(int y) {
 	counter--;
 }
 
-template <class j>
+template <typename j>
 DynamicList<j>::DynamicList()
 {
 	counter = 0;
@@ -30,7 +32,7 @@ DynamicList<j>::DynamicList()
 	listSize = 8;
 }
 
-template <class j>
+template <typename j>
 void DynamicList<j>::push(j x) {
 	if ((listSize - 1) > counter) {
 		list[counter] = x;
@@ -48,13 +50,35 @@ void DynamicList<j>::push(j x) {
 	}
 }
 
-template <class j>
+template <typename j>
 int DynamicList<j>::length() {
 	return counter;
 }
 
-template <class j>
+template <typename j>
 int DynamicList<j>::get(int i) {
 	return list[i];
+}
+
+template <typename j>
+int DynamicList<j>::index(j y) {
+	int realValue;
+	for (int c = 0; c < counter; c++) {
+		if (list[c] == y) {
+			realValue = c;
+		}
+	}
+	return realValue;
+}
+
+template <typename j>
+bool DynamicList<j>::contains(j z) {
+	bool containValue = false;
+	for (int c = 0; c < counter; c++) {
+		if (list[c] == z) {
+			containValue = true;
+		}
+	}
+	return containValue;
 }
 
